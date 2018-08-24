77c4a9f25071c5a9312c9b7a4ee92d9b49b71823
lidehua <lidehua@lsrobot.vip>
2018-08-24 18:35:47 +0800

    代码生成

--- src/main/java/com/ldh/RedisAnnotationApplication.java
+++ src/main/java/com/ldh/RedisAnnotationApplication.java
@@ -1,9 +1,14 @@
 package com.ldh;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @Author: ldh
+ */
 @SpringBootApplication
+@MapperScan("com.ldh.dao*")
 public class RedisAnnotationApplication {
 
 	public static void main(String[] args) {
